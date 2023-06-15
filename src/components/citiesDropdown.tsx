75a351e4e1452ffccedaf90772b151b7ba0e93e0
Kweeka <mtalbi29@yahoo.com>
2023-06-15 20:43:30 +0100

    Add citiesDropdown component

--- src/components/citiesDropdown.tsx
+++ src/components/citiesDropdown.tsx
@@ -1,28 +1,42 @@
-"use client"
+const cities = [
+  "Atlanta Metro",
+  "Austin",
+  "Baltimore",
+  "Chicago / Illinois",
+  "Dallas Fort Worth",
+  "Denver",
+  "Detroit / Eastern Michigan",
+  "Houston",
+  "Los Angeles",
+  "Miami",
+  "New York",
+]
 
-import {
-  NavigationMenu,
-  NavigationMenuContent,
-  NavigationMenuItem,
-  NavigationMenuLink,
-  NavigationMenuTrigger,
-} from "@/components/ui/navigation-menu"
-import { HiOutlineLocationMarker } from "react-icons/hi"
-
-const CitiesDropdown = ({ city }: { city: string }) => {
+const CitiesDropdown = () => {
   return (
-    <div>
-      <NavigationMenu>
-        <NavigationMenuItem>
-          <NavigationMenuTrigger className="gap-2">
-            <HiOutlineLocationMarker size={24} />
-            <p>{city}</p>
-          </NavigationMenuTrigger>
-          <NavigationMenuContent>
-            <NavigationMenuLink>Link</NavigationMenuLink>
-          </NavigationMenuContent>
-        </NavigationMenuItem>
-      </NavigationMenu>
+    <div className="w-[650px] h-[450px] flex flex-col">
+      <div className="flex h-full">
+        <div className="w-full">
+          <div className="p-4 border-b border-gray-200">
+            <p className="text-[16px] font-medium">Metro</p>
+          </div>
+          <div className="h-[87%] overflow-x-hidden overflow-y-scroll border-r border-gray-200">
+            {cities.map((city) => (
+              <p className="text-[16px] text-gray-500 py-3 px-3 cursor-pointer border-l-4 border-l-white hover:text-[#247f9e] hover:bg-[#247f9e20] hover:border-l-4 hover:border-l-[#247f9e]" key={city}>{city}</p>
+            ))}
+          </div>
+        </div>
+        <div className="w-full">
+          <div className="p-4 border-b border-gray-200">
+            <p className="text-[16px] font-medium">Region</p>
+          </div>
+          <div className="h-[87%] overflow-x-hidden overflow-y-scroll">
+            {cities.map((city) => (
+              <p className="text-[16px] text-gray-500 py-3 px-3 cursor-pointer border-l-4 border-l-white hover:text-[#247f9e] hover:bg-[#247f9e20] hover:border-l-4 hover:border-l-[#247f9e]" key={city}>{city}</p>
+            ))}
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
