6fe3553f4854508048262e254fe1d4e663f143d7
kweeka <mtalbi29@yahoo.com>
2023-06-20 21:32:51 +0100

    Update breadcrumbs functionality

--- src/components/BreadCrumbs.tsx
+++ src/components/BreadCrumbs.tsx
@@ -1,10 +1,17 @@
 import BreadCrumb from '@/components/ui/BreadCrumb';
+import { capitalize } from '@/lib/utils';
 
-const BreadCrumbs = () => {
+const BreadCrumbs = ({ pathSegments }: { pathSegments: Array<{ name: string; path: string }> }) => {
   return (
     <div className="w-full h-8 bg-gray-50 px-12 py-1 flex gap-2">
-      <BreadCrumb path="Home" />
-      <BreadCrumb path="Netherlands" />
+      {
+        pathSegments.map((segment) =>
+          <BreadCrumb 
+            path={segment.path} 
+            name={capitalize(segment.name.replace(/-/g, ' '))}
+            key={segment.path} />
+        )
+      }
     </div>
   )
 }
