7cbc5befa5b0ef847b066a0c9a52ccfeafb8ed79
kweeka <mtalbi29@yahoo.com>
2023-06-23 18:41:15 +0100

    Use popover instead of navigationMenu

--- src/components/citiesNavLink.tsx
+++ src/components/citiesNavLink.tsx
@@ -1,32 +1,31 @@
 "use client"
 
-import {
-  NavigationMenu,
-  NavigationMenuContent,
-  NavigationMenuItem,
-  NavigationMenuLink,
-  NavigationMenuTrigger,
-} from "@/components/ui/navigation-menu"
-import { HiOutlineLocationMarker } from "react-icons/hi"
 import CitiesDropdown from "./citiesDropdown"
+import { Button } from "@/components/ui/button"
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover"
+import { ChevronDown, MapPin } from "lucide-react"
 
 const CitiesNavLink = ({ city }: { city: string }) => {
   return (
-    <div>
-      <NavigationMenu>
-        <NavigationMenuItem>
-          <NavigationMenuTrigger className="gap-[1px]">
-            <HiOutlineLocationMarker size={24} />
-            <p>{city}</p>
-          </NavigationMenuTrigger>
-          <NavigationMenuContent>
-            <NavigationMenuLink>
-              <CitiesDropdown />
-            </NavigationMenuLink>
-          </NavigationMenuContent>
-        </NavigationMenuItem>
-      </NavigationMenu>
-    </div>
+    <Popover>
+      <PopoverTrigger className="px-3 py-2 w-auto" asChild>
+        <Button
+          variant={"borderless"}
+          className="justify-start text-left font-normal"
+        >
+          <MapPin />
+          <p className="mx-1">{city}</p>
+          <ChevronDown size={14} />
+        </Button>
+      </PopoverTrigger>
+      <PopoverContent className="w-auto p-0 absolute left-[-8rem]">
+        <CitiesDropdown />
+      </PopoverContent>
+    </Popover>
   )
 }
 
