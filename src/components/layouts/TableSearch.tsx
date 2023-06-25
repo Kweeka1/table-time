2c017a19147638d470a941339912e7bc4b6661fd
kweeka <mtalbi29@yahoo.com>
2023-06-25 09:45:54 +0100

    Update tableSearch components

--- src/components/layouts/TableSearch.tsx
+++ src/components/layouts/TableSearch.tsx
@@ -2,7 +2,7 @@ import SearchForm from "../SearchForm";
 
 const TableSearch = () => {
   return (
-    <div className="w-full h-[45vh] bg-gradient-to-r from-[#132049] to-[#677089] flex flex-col gap-8 items-center justify-center">
+    <div className="w-full h-[50vh] bg-gradient-to-r from-[#132049] to-[#677089] flex flex-col gap-8 items-center justify-center">
         <h1 className="text-5xl text-white font-bold">Find your table for any occasion</h1>
         <div className="flex gap-4">
           <SearchForm />
--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -3,20 +3,29 @@ import * as React from "react"
 import { cn } from "@/lib/utils"
 
 export interface InputProps
-  extends React.InputHTMLAttributes<HTMLInputElement> {}
+  extends React.InputHTMLAttributes<HTMLInputElement> {
+    icon?: React.ReactNode
+  }
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
-  ({ className, type, ...props }, ref) => {
+  ({ className, type, icon, ...props }, ref) => {
     return (
-      <input
-        type={type}
-        className={cn(
-          "flex h-12 w-full rounded-md border border-input bg-transparent px-3 py-2 text-sm file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground disabled:cursor-not-allowed disabled:opacity-50 focus-visible:outline-none",
-          className
+      <div className="relative w-full">
+        {icon && (
+          <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+            {icon}
+          </div>
         )}
-        ref={ref}
-        {...props}
-      />
+        <input
+          type={type}
+          className={cn(
+            `flex h-12 w-full rounded-md border border-input bg-transparent py-2 ${icon ? "pl-10" : "pl-3"} pr-3 text-sm file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground disabled:cursor-not-allowed disabled:opacity-50 focus-visible:outline-none`,
+            className,
+          )}
+          ref={ref}
+          {...props}
+        />
+      </div>
     )
   }
 )
